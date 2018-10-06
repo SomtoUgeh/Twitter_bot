e4879f39eadb2daa7dc2b33f025dd9e2bf257f8b
somtougeh <smugeh@gmail.com>
2018-10-06 09:19:24 +0100

    Renamed functions

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const config = require("./config.js");
 
 let T = new Twit(config);
 
-setInterval(tweetIt, 1000 * 2);
+setInterval(postTweet, 1000 * 2);
 
 function getTweet() {
 	let params = { q: "Ugeh since:2017-07-18", count: 100 };
@@ -17,7 +17,7 @@ function getTweet() {
 	T.get("search/tweets", params, getData);
 }
 
-function tweetIt() {
+function postTweet() {
 	let r = Math.floor(Math.random() * 100);
 	let tweet = { status: "hello world!, first bot in coming #" + r };
 	let tweeted = (err, data, response) => {
